981c4728995f51ee33c4249ff27f7cdedd34a43e
noPnoG <283326984@qq.com>
2021-06-23 23:05:08 +0800

    play

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,19 +1,38 @@
 <template>
   <div class="player">
-      <div class="normal-player" v-show="fullScreen">
-          <div class="background">
-              <img :src="currentSong.pic" >
-          </div>
-          <div class="top">
-              <div class="back" @click="goback">
-                  <i class="icon-back"></i>
-              </div>
-              <h1 class="title">{{currentSong.name}}</h1>
-              <h1 class="subtitle">{{currentSong.singer}}</h1>
-          </div>
+    <div class="normal-player" v-show="fullScreen">
+      <div class="background">
+        <img :src="currentSong.pic" />
       </div>
+      <div class="top">
+        <div class="back" @click="goback">
+          <i class="icon-back"></i>
+        </div>
+        <h1 class="title">{{ currentSong.name }}</h1>
+        <h1 class="subtitle">{{ currentSong.singer }}</h1>
+      </div>
+      <div class="bottom">
+        <div class="operators">
+          <div class="icon i-left">
+            <i class="icon-sequence"></i>
+          </div>
+          <div class="icon i-left" :class="disableCls">
+            <i @click="prev" class="icon-prev"></i>
+          </div>
+          <div class="icon i-center" :class="disableCls">
+            <i @click="togglePlay" :class="playIcon"></i>
+          </div>
+          <div class="icon i-right" :class="disableCls">
+            <i @click="next" class="icon-next"></i>
+          </div>
+          <div class="icon i-right">
+            <i class="icon-not-favorite"></i>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  <audio ref="audioRef"></audio>
+  <audio ref="audioRef" @pause="pause" @canplay="ready" @error="error"></audio>
 </template>
 
 <script>
@@ -24,84 +43,252 @@ export default {
   setup () {
     const store = useStore()
     const audioRef = ref(null)
+    const songReady = ref(false)
     const fullScreen = computed(() => store.state.fullScreen)
     const currentSong = computed(() => store.getters.currentSong)
-
-    watch(currentSong, (newSong) => {
-      if (!newSong.id || !newSong.url) { return }
+    const playing = computed(() => store.state.playing)
+    const playList = computed(() => store.state.playList)
+    const currentIndex = computed(() => store.state.currentIndex)
+    const playIcon = computed(() => {
+      return playing.value ? 'icon-pause' : 'icon-play'
+    })
+    const disableCls = computed(() => {
+      return songReady.value ? '' : 'disable'
+    })
+    watch(currentSong, newSong => {
+      if (!newSong.id || !newSong.url) {
+        return
+      }
+      songReady.value = false
       const audioEl = audioRef.value
       audioEl.src = newSong.url
       audioEl.play()
     })
+
+    watch(playing, newPlaying => {
+      if (!songReady.value) {
+        return
+      }
+      const audioEl = audioRef.value
+      newPlaying ? audioEl.play() : audioEl.pause()
+    })
+
+    function togglePlay () {
+      if (!songReady.value) {
+        return
+      }
+      store.commit('setPlayingState', !playing.value)
+    }
     function goback () {
       store.commit('setFullScreen', false)
+    }
+
+    function pause () {
+      store.commit('setPlayingState', false)
+    }
+
+    function prev () {
+      let index = currentIndex.value - 1
+      const list = playList.value
+      if (!songReady.value || !list.length) {
+        return
+      }
+      if (list.length === 1) {
+        loop()
+      } else {
+        if (index === -1) {
+          index = list.length - 1
+        }
+        store.commit('setCurrentIndex', index)
+        if (!playing.value) {
+          store.commit('setPlayingState', true)
+        }
+      }
+    }
+    function next () {
+      let index = currentIndex.value + 1
+      const list = playList.value
+      if (!songReady.value || !list.length) {
+        return
+      }
+      if (list.length === 1) {
+        loop()
+      } else {
+        if (index === list.length) {
+          index = 0
+        }
+        store.commit('setCurrentIndex', index)
+        if (!playing.value) {
+          store.commit('setPlayingState', true)
+        }
+      }
+    }
+
+    function loop () {
+      const audioEl = audioRef.value
+      audioEl.currenTime = 0
+      audioEl.play()
+    }
+
+    function ready () {
+      if (songReady.value) {
+        return
+      }
+      songReady.value = true
+    }
+    function error () {
+      songReady.value = true
     }
     return {
       fullScreen,
       currentSong,
       audioRef,
-      goback
+      goback,
+      playIcon,
+      togglePlay,
+      pause,
+      next,
+      prev,
+      ready,
+      disableCls,
+      error
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
- .player{
-     .normal-player{
-         position: fixed;
-         top: 0;
-         right: 0;
-         bottom:0;
-         left:0;
-         z-index: 150;
-         background: $color-background;
-     }
-     .background{
-         position: absolute;
-         left: 0;
-         top: 0;
-         width: 100%;
-         height: 100%;
-         z-index: -1;
-         opacity: 0.6;
-         filter: blur(20px);
-         img{
-             width: 100%;
-             height: 100%;
-         }
-     }
-     .top {
-        position: relative;
-        margin-bottom: 25px;
-        .back {
-          position: absolute;
-          top: 0;
-          left: 6px;
-          z-index: 50;
-        }
-        .icon-back {
-          display: block;
-          padding: 9px;
-          font-size: $font-size-large-x;
-          color: $color-theme;
-          transform: rotate(-90deg);
-        }
-        .title {
-          width: 70%;
-          margin: 0 auto;
-          line-height: 40px;
-          text-align: center;
-          @include no-wrap();
-          font-size: $font-size-large;
-          color: $color-text;
-        }
-        .subtitle {
-          line-height: 20px;
-          text-align: center;
-          font-size: $font-size-medium;
-          color: $color-text;
+.player {
+  .normal-player {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 150;
+    background: $color-background;
+  }
+  .background {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    opacity: 0.6;
+    filter: blur(20px);
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+  .top {
+    position: relative;
+    margin-bottom: 25px;
+    .back {
+      position: absolute;
+      top: 0;
+      left: 6px;
+      z-index: 50;
+    }
+    .icon-back {
+      display: block;
+      padding: 9px;
+      font-size: $font-size-large-x;
+      color: $color-theme;
+      transform: rotate(-90deg);
+    }
+    .title {
+      width: 70%;
+      margin: 0 auto;
+      line-height: 40px;
+      text-align: center;
+      @include no-wrap();
+      font-size: $font-size-large;
+      color: $color-text;
+    }
+    .subtitle {
+      line-height: 20px;
+      text-align: center;
+      font-size: $font-size-medium;
+      color: $color-text;
+    }
+  }
+  .bottom {
+    position: absolute;
+    bottom: 50px;
+    width: 100%;
+    .dot-wrapper {
+      text-align: center;
+      font-size: 0;
+      .dot {
+        display: inline-block;
+        vertical-align: middle;
+        margin: 0 4px;
+        width: 8px;
+        height: 8px;
+        border-radius: 50%;
+        background: $color-text-l;
+        &.active {
+          width: 20px;
+          border-radius: 5px;
+          background: $color-text-ll;
         }
       }
- }
+    }
+    .progress-wrapper {
+      display: flex;
+      align-items: center;
+      width: 80%;
+      margin: 0px auto;
+      padding: 10px 0;
+      .time {
+        color: $color-text;
+        font-size: $font-size-small;
+        flex: 0 0 40px;
+        line-height: 30px;
+        width: 40px;
+        &.time-l {
+          text-align: left;
+        }
+        &.time-r {
+          text-align: right;
+        }
+      }
+      .progress-bar-wrapper {
+        flex: 1;
+      }
+    }
+    .operators {
+      display: flex;
+      align-items: center;
+      .icon {
+        flex: 1;
+        color: $color-theme;
+        &.disable {
+          color: $color-theme-d;
+        }
+        i {
+          font-size: 30px;
+        }
+      }
+      .i-left {
+        text-align: right;
+      }
+      .i-center {
+        padding: 0 20px;
+        text-align: center;
+        i {
+          font-size: 40px;
+        }
+      }
+      .i-right {
+        text-align: left;
+      }
+      .icon-favorite {
+        color: $color-sub-theme;
+      }
+    }
+  }
+}
 </style>
