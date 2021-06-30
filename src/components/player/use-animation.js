bf2c2427d00fc4bbf42767c4249864f7dc5ff014
noPnoG <283326984@qq.com>
2021-06-30 17:28:53 +0800

    null

---
+++ src/components/player/use-animation.js
@@ -0,0 +1,22 @@
+
+import animations from 'create-keyframe-animation'
+export default function useAnimation() {
+  function enter () {
+
+  }
+  function afterEnter () {
+
+  }
+  function leave () {
+
+  }
+  function afterLeave () {
+
+  }
+  return {
+    enter,
+    afterEnter,
+    leave,
+    afterLeave
+  }
+}
