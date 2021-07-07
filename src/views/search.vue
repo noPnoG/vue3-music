3fbc284c862aaf6c8c9b97663e77863626ba0274
noPnoG <283326984@qq.com>
2021-07-07 14:23:39 +0800

    null

--- src/views/search.vue
+++ src/views/search.vue
@@ -1,10 +1,24 @@
 <template>
-  <div>search</div>
+  <div>
+    <search-input v-model="query"  placeholder="搜索"></search-input>
+    {{query}}
+  </div>
 </template>
 
 <script>
+import SearchInput from '@/components/search/search-input'
 export default {
-
+  components: {
+    SearchInput
+  },
+  data () {
+    return { query: '' }
+  },
+  methods: {
+    onInput (value) {
+      console.log('update:query', this.query)
+    }
+  }
 }
 </script>
 
